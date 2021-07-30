a8db797297d51c78e0dd685a80d030ac98f2a6ec
sayakass <sayaka.jp@icloud.com>
2021-07-30 19:32:13 +0700

    lab2 2nd time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Everything is clear, you can start now.</div>;
+  return (
+    <div className="App">
+      <p>
+        <label htmlFor="province">
+          Province: &nbsp;
+          <select id="province">
+            <option value="">- Select Province -</option>
+            <option value="Bangkok">Bangkok</option>
+            <option value="Nonthaburi">Nonthaburi</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label htmlFor="district">
+          District: &nbsp;
+          <select id="district">
+            <option value="">- Select District -</option>
+            <option value="Bangkok">Pak kret</option>
+            <option value="Nonthaburi">Mueang Nonthaburi</option>
+          </select>
+        </label>
+      </p>
+    </div>
+  );
 }
 
 export default App;
