159be02ebf5638dc35379ca2f148361262f0c7b2
sayakass <sayaka.jp@icloud.com>
2021-08-01 18:44:56 +0700

    clear

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <button>Add</button>
-      <div>
-        <span style={{ textDecoration: 'line-through' }}>Task1</span>
-        <button>Edit</button>
-        <button>Del</button>
-      </div>
-      <div>
-        <span style={{ textDecoration: 'line-through' }}>Task2</span>
-        <button>Edit</button>
-        <button>Del</button>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
